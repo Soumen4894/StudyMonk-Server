1e6e5ce02497b5cf5ccf4711321fd7527bc3fa0e
Soumen4894 <soumenmandal0008@gmail.com>
2024-07-30 22:29:24 +0530

    hvb

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use(function (req, res, next) {
 	  "Origin, X-Requested-With, Content-Type, Accept"
 	);
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header('Access-Control-Allow-Credentials', 'true');
 	next();
 });
 
