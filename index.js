1b88ed193ae16be4e19dcc747db3eabfd703c9b2
Soumen4894 <soumenmandal0008@gmail.com>
2024-07-30 18:27:03 +0530

    cors issues

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ app.use(cookieParser());
 // 	//}
 // )
 // )
+app.use(cors());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+	  "Access-Control-Allow-Headers",
+	  "Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	next();
+});
+
 app.get("/", (req, res) => {
 	return res.json({
 		success:true,
