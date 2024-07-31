8310add8abb9b9246965970094d27ff32fe126c4
Soumen4894 <soumenmandal0008@gmail.com>
2024-07-31 19:44:54 +0530

    changes

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-monk-client-tq27.vercel.app/",
 		credentials:true,
 	})
 )
