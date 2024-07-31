123aecbafefeb12113cd982d1209b20296b79cd4
Soumen4894 <soumenmandal0008@gmail.com>
2024-07-31 10:44:19 +0530

    sdf

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    //origin:'https://study-monk-client-tq27.vercel.app',
-	origin:'*', 
-    credentials:true,           
-    optionSuccessStatus:200
-}));
-//app.use(cors());
+// app.use(cors({
+//     //origin:'https://study-monk-client-tq27.vercel.app',
+// 	origin:'*', 
+//     credentials:true,           
+//     optionSuccessStatus:200
+// }));
+app.use(cors());
 
 
 app.use(function (req, res, next) {
