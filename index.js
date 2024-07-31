4cfbd2634bf7b685fc00a9533af1dd867147e66d
Soumen4894 <soumenmandal0008@gmail.com>
2024-07-31 09:55:08 +0530

    fsd

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:'https://study-monk-server-phi.vercel.app/', 
+    //origin:'https://study-monk-server-phi.vercel.app/',
+	origin:'*', 
     credentials:true,           
     optionSuccessStatus:200
 }));
